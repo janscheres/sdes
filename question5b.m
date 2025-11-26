43d80ee8ba14b7d79c8320395e743351ac08862c
Jan Scheres Monesma <jscheresm@gmail.com>
2025-11-26 12:07:30 +0000

    Bigger fonts for q5b

--- question5b.m
+++ question5b.m
@@ -1,11 +1,6 @@
-% Paramters (subject to variability) 
-
 T = 50;
 dt = 0.01;
 X_0 = 0.5;
-
-
-
 
 c_values = [0.3, 0.5, 1];
 
@@ -25,23 +20,6 @@ for j = 1:length(c_values)
 
     subplot(1, length(c_values), j);
     histogram(X_end, 20);
-    title(['c = ', num2str(c)]);
-    xlabel('X(T)'); ylabel('Counts');
-end
-
-function [X, t] = euler_maruyama(f, g, T, dt, X0)
-
-N = floor(T/dt);%number of timesteps in total
-t = 0:dt:T;
-
-X = zeros(1, N+1);%answer will be in this
-X(1) = X0;
-
-R = randn(1, N);%get random numbers as vector
-dW = sqrt(dt) * R;
-
-for i = 1:N
-    X(i+1) = X(i) + f(X(i)) * dt + g(X(i)) * dW(i);
-end
-
+    title(['$c = ', num2str(c), '$'], 'Interpreter', 'latex', 'FontSize', 18);
+    xlabel('$X(T)$', 'Interpreter', 'latex', 'FontSize', 14); ylabel('Counts', 'FontSize', 14);
 end
