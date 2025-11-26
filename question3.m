f56abc50d405bd683c5480b7f596b89353c142bd
Jan Scheres Monesma <jscheresm@gmail.com>
2025-11-26 11:46:55 +0000

    Bigger fonts for question 3

--- question3.m
+++ question3.m
@@ -18,27 +18,7 @@ for j = 1:length(dt_values)
     end
     yline(0, 'k--', 'LineWidth', 1.5);
 
-    title(sprintf('$ \\Delta t = %.2f$', dt), 'Interpreter', 'latex', 'FontSize', 14);
-    xlabel('$t$', 'Interpreter', 'latex'); ylabel('$X(t)$', 'Interpreter', 'latex');
+    title(sprintf('$ \\Delta t = %.2f$', dt), 'Interpreter', 'latex', 'FontSize', 18);
+    xlabel('$t$', 'Interpreter', 'latex', 'FontSize', 14); ylabel('$X(t)$', 'Interpreter', 'latex', 'FontSize', 14);
     grid on;
-end
-
-
-
-
-function [X, t] = euler_maruyama(f, g, T, dt, X0)
-
-N = floor(T/dt);%number of timesteps in total
-t = 0:dt:T;
-
-X = zeros(1, N+1);%answer will be in this
-X(1) = X0;
-
-R = randn(1, N);%get random numbers as vector
-dW = sqrt(dt) * R;
-
-for i = 1:N
-    X(i+1) = X(i) + f(X(i)) * dt + g(X(i)) * dW(i);
-end
-
 end
