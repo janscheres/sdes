dbc75e4e2ece3f30e47e96d74348f27393ee90ca
Jan Scheres Monesma <jscheresm@gmail.com>
2025-11-26 12:56:07 +0000

    Last plots from 5a

--- question5a.m
+++ question5a.m
@@ -49,3 +49,23 @@ hold off;
 xlabel('$t$', 'Interpreter', 'latex', 'FontSize', 14);
 ylabel('$X(t)$', 'Interpreter', 'latex', 'FontSize', 14);
 title('$X_0 = -0.5$, $c = 0.2$', 'Interpreter', 'latex', 'FontSize', 18);
+
+%next plot
+X0_values = [0.5, -0.5, 5];
+figure;
+
+for j = 1:length(X0_values)
+    %rng(1); used to get same plots as Ben
+    X_0_current = X0_values(j);
+    
+    [X, t] = euler_maruyama(f, @(x) 0.5, T, dt, X_0_current);
+    
+    subplot(1, 3, j);
+    plot(t, X, 'r', 'LineWidth', 1.5);
+    hold on;
+    yline(0, '--k', 'LineWidth', 2.0);
+    hold off;
+    xlabel('$t$', 'Interpreter', 'latex', 'FontSize', 14);
+    ylabel('$X(t)$', 'Interpreter', 'latex', 'FontSize', 14);
+    title(['$X_0 = ', num2str(X_0_current), '$, $c = 0.5$'], 'Interpreter', 'latex', 'FontSize', 18);
+end
